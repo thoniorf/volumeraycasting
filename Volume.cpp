2d1ef6068a7f7fc725eebf1ac6619ab7e55101b6
Antonio Fortino <thoniorf@gmail.com>
2019-03-31 21:28:55 +0200

    Added camera rotation still bugged

--- Volume.cpp
+++ Volume.cpp
@@ -49,10 +49,10 @@ public:
 double degToRad(double d) {
 	return d * M_PI / 180;
 }
-
+inline 
 double fixZeroDoublePrecisionError(double x) {
 	if (std::fabs(x) <= 5.00e-5)
-		return 0.0;
+		return 0;
 	return x;
 }
 /* 3D VECTOR CLASS */
@@ -61,7 +61,7 @@ public:
 	double x, y, z;
 	Vector3() { x = y = z = 0; }
 	Vector3(double scalar) : x(scalar), y(scalar), z(scalar) {}
-	Vector3(double i, double j, double k) : x(i), y(j), z(k){}
+	Vector3(double i, double j, double k) : x(fixZeroDoublePrecisionError(i)), y(fixZeroDoublePrecisionError(j)), z(fixZeroDoublePrecisionError(k)){}
 
 	const double& operator [] (size_t i) const { return (&x)[i]; }
 	double& operator [] (size_t i) { return (&x)[i]; }
@@ -217,6 +217,9 @@ public:
 	Grid() {}
 	Grid(const Vector3 &min, const Vector3 &max) {
 		bounds[0] = min, bounds[1] = max;
+	} 
+	bool isOutsideGrid(int ix, int iy, int iz) {
+		return ix < 0 || iy < 0 || iz < 0 || ix > bounds[1].x - 1 || iy > bounds[1].y - 1 || iz > bounds[1].z - 1;
 	}
 	friend std::ostream& operator << (std::ostream &s, const Grid &g)
 	{
@@ -278,11 +281,12 @@ public:
 	Vector3 from;
 	Vector3 to;
 	Vector3 forward;
+	Vector3 backward;
 	Vector3 right;
 	Vector3 up;
 	Matrix4x4 lookAt;
 	Matrix4x4 worldTransformation;
-
+	double theta, phi;
 	Camera(){}
 	Camera(Vector3 center) {
 		this->center = center;
@@ -292,32 +296,67 @@ public:
 	Camera(Vector3 from, Vector3 to ) {
 		this->from = from;
 		this->to = to;
-		
 		setupLookAtMatrix();
 	}
 	void yaw(double angle) { // z-axis
-		double theta = degToRad(angle);
-
-		Matrix4x4 r = Matrix4x4(std::cos(theta), -1* std::sin(theta), 0, 0,
+		stream << "YAW" << std::endl;
+		stream << "from: " << from << std::endl;
+		backward = from - to;
+		theta = degToRad(angle);
+		stream << "backward: " << backward << std::endl;
+		stream << "angle: " << angle << " in rad: " << theta << std::endl;
+		Matrix4x4 rZ = Matrix4x4(std::cos(theta), -1 * std::sin(theta), 0, 0,
 			std::sin(theta), std::cos(theta), 0, 0,
 			0, 0, 1, 0,
 			0, 0, 0, 1);
-		from = r.VecMatrixMulti(from);
+		
+		stream << backward << " ";
+		backward = rZ.DirMatrixMulti(backward);
+
+		stream << backward << " "<<std::endl;
+		from = to + backward;
+
+		stream << "from: " << from<<std::endl;
 		setupLookAtMatrix();
 	}
 	void pitch(double angle) { //y-axis
-		double theta = degToRad(angle);
-		Matrix4x4 r = Matrix4x4(std::cos(theta), 0, std::sin(theta), 0,
+		stream << "PITCH" << std::endl;
+		stream << "from: " << from << std::endl;
+		backward = from - to;
+		phi = degToRad(angle);
+		stream << "backward: " << backward << std::endl;
+		stream << "angle: " << angle << " in rad: " << theta << std::endl;
+		Matrix4x4 rY = Matrix4x4(std::cos(phi), 0, std::sin(phi), 0,
 			0, 1, 0, 0,
-			-1*std::sin(theta), 0, std::cos(theta), 0,
+			-1 * std::sin(phi), 0, std::cos(phi), 0,
 			0, 0, 0, 1);
-		from = r.VecMatrixMulti(from);
+
+		stream << backward << " ";
+		backward = rY.DirMatrixMulti(backward);
+
+		stream << backward << " " << std::endl;
+		from = to + backward;
+
+		stream << "from: " << from << std::endl;
 		setupLookAtMatrix();
+	}
+
+	void roll(double angle) {
+		Matrix4x4 rX = Matrix4x4(1, 0, 0, 0,
+			0, std::cos(theta), -1 * std::sin(theta), 0,
+			0, std::sin(theta), std::cos(theta), 0,
+			0, 0, 0, 1);
 	}
 private:
 	void setupLookAtMatrix() {
 		Vector3 tmp(0, 1, 0);
 		forward = (to - from).normalize();
+		if(forward.y == 1) {
+			tmp = Vector3(1, 0, 0);
+		}
+		else if (forward.y == -1) {
+			tmp = Vector3(-1, 0, 0);
+		}
 		right = tmp.cross(forward);
 		up = forward.cross(right);
 
@@ -388,6 +427,7 @@ double interpolation(double x,Options option) {
 	if (x < option.threshold) return option.minIntensity;
 	return  (x - option.minIntensity) * ((255) / (option.maxIntensity - option.minIntensity));
 }
+
 Vector3 rasterToScreen(size_t w, size_t h, Options options) {
 	// from raster to normalized to normalized to screen to camera
 	double cameraX = (2 * (w + 0.5) / options.imageWidth - 1) * options.scale * options.imageAspectRatio;
@@ -404,16 +444,49 @@ Vector3 rasterToScreen(size_t w, size_t h,double z,Options options) {
 	return Vector3(cameraX, cameraY, z);
 }
 
+
 class MexFunction : public matlab::mex::Function {
     ArrayFactory factory;
     std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
-	//std::ostringstream stream;
 	Options options;
 
+
 public:
+	void debugAffineTransformation() {
+		const double theta = degToRad(90);
+
+		Matrix4x4 rZ = Matrix4x4(std::cos(theta), -1* std::sin(theta), 0, 0,
+			std::sin(theta), std::cos(theta), 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1);
+		Matrix4x4 rY = Matrix4x4(std::cos(theta), 0, std::sin(theta), 0,
+			0, 1, 0, 0,
+			-1*std::sin(theta), 0, std::cos(theta), 0,
+			0, 0, 0, 1);
+		Matrix4x4 rX = Matrix4x4(1, 0, 0, 0,
+			0, std::cos(theta), -1*std::sin(theta), 0,
+			0, std::sin(theta), std::cos(theta), 0,
+			0, 0, 0, 1);
+		Vector3 v1(1, 0, 0);
+		Vector3 v2(0, 0, 1);
+		Vector3 v3(0, 1, 0);
+
+		Vector3 vz = rZ.DirMatrixMulti(v1);
+		Vector3 vy = rY.DirMatrixMulti(v2);
+		Vector3 vx = rX.DirMatrixMulti(v3);
+		stream << "DEBUG AFFINE TRANSFORMATION" << std::endl;
+		stream << vz << std::endl;
+		stream << vy << std::endl;
+		stream << vx << std::endl;
+		stream << "END DEBUG" << std::endl;
+		displayOnMATLAB(stream);
+	}
+
 	void operator()(ArgumentList outputs, ArgumentList inputs) {
-		checkArguments(inputs);
 		std::time(&timer);
+		//debugAffineTransformation();
+		checkArguments(inputs);
+
 		const TypedArray<double> vol_size	= inputs[0];
 		const TypedArray<double> volume		= inputs[1];
 		const TypedArray<double> viewSize	= inputs[2];
@@ -440,24 +513,19 @@ public:
 		Grid grid(Vector3(0), Vector3(vol_size[0], vol_size[1], vol_size[2]));
 	
 		Camera camera(Vector3(vol_size[0] / 2 + options.viewOffset, vol_size[1] / 2, vol_size[2] / 2 ), Vector3(vol_size[0]/2, vol_size[1]/2, vol_size[2]/2));
-
-		camera.pitch(rotation[1]);
 		camera.yaw(rotation[0]);
-
-		//print lookat matrix
+		camera.pitch(rotation[1]);
+		displayOnMATLAB(stream);
 
 		int maxIteration = std::floor(std::fmax(vol_size[0], std::fmax(vol_size[1], vol_size[2])));
 		int counterNoIntersection = 0;
 		int counterIntersection = 0;
 
-		stream << "grid" << grid << std::endl;
-		/*stream << camera.lookAt << std::endl;		
-		stream << rayStart << std::endl;*/
+		stream << camera.lookAt << std::endl;		
 
-		std::ofstream oflog("./info.log"); // DEBUG INFO 
-		std::ofstream ofs("./out.ppm", std::ios::out | std::ios::binary); //DEBUG IMAGE
 		
-		double littleStep = 0.2;
+		
+		double littleStep = 0.5;
 
 		double halfWidth = std::fabs(options.imageWidth / 2);
 		double halfHeight = std::fabs(options.imageHeight / 2);
@@ -465,16 +533,12 @@ public:
 		for (size_t h = 0; h < options.imageHeight; ++h) {
 			for (size_t w = 0; w < options.imageWidth; ++w) {
 
-				//Vector3 rayStart = camera.lookAt.VecMatrixMulti(rasterToScreen(w,h, 0,options));
-				//Vector3 rayEnd = camera.lookAt.VecMatrixMulti(rasterToScreen(w, h, 1, options));
 				double x = w - halfWidth;
 				double y = h - halfHeight;
 
 				Vector3 rayStart = camera.lookAt.VecMatrixMulti(Vector3(x,y,0));
 				Vector3 rayEnd = camera.lookAt.VecMatrixMulti(Vector3(x,y, 1));
 				
-				/*stream <<"rayStart: "<< rayStart << std::endl;
-				stream <<"rayend: "<< rayEnd << std::endl;*/
 
 				Ray ray(rayStart, rayEnd);
 
@@ -491,8 +555,12 @@ public:
 					Vector3 start = ray.orig + ray.dir *hit.tmin;
 					Vector3 end = ray.orig + ray.dir *hit.tmax;
 
-					displayOnMATLAB(stream);
+					int ix = std::floor(start.x) != 0 ? std::floor(start.x) - 1 : std::floor(start.x);
+					int iy = std::floor(start.y) != 0 ? std::floor(start.y) - 1 : std::floor(start.y);
+					int iz = std::floor(start.z) != 0 ? std::floor(start.z) - 1 : std::floor(start.z);
+
 					while (start != end ) {
+
 						if  (time(NULL) - timer >= timeout)
 						{
 							stream << "TIMEOUT" << std::endl;
@@ -503,18 +571,28 @@ public:
 						int iy = std::floor(start.y) != 0 ? std::floor(start.y) - 1 : std::floor(start.y);
 						int iz = std::floor(start.z) != 0 ? std::floor(start.z) - 1 : std::floor(start.z);
 
-						if (ix < 0 || iy < 0 || iz < 0 || ix > grid.bounds[1].x-1 || iy > grid.bounds[1].y-1 || iz > grid.bounds[1].z-1) {
-							//stream << start << std::endl;
-							//stream << std::floor(start.x) << " " << std::floor(start.y) << " " << std::floor(start.z) << std::endl;
-							//stream << ix << " " << iy << " " << iz << std::endl;
-							//displayOnMATLAB(stream);
-							break;
-						}
-						//add try-catch no 
+						if (grid.isOutsideGrid(ix,iy,iz)) break;
+
 						try {
-							double value = volume[ix][iy][iz];
-							if (value >= options.threshold) {
-								viewOutput[w][h] = interpolation(value, options);
+							int up		= iy + 1;
+							int down	= iy - 1;
+							int front	= iz + 1;
+							int back	= iz - 1;
+							int left	= ix + 1;
+							int right	= ix - 1;
+							
+							double up_voxel		= (up < vol_size[1] - 1) ? volume[ix][up][iz]			: 0.0;
+							double down_voxel	= (down >= 0) ? (volume[ix][down][iz])					: 0.0;
+							double front_voxel	= (front < vol_size[2] - 1) ? (volume[ix][iy][front])	: 0.0;
+							double back_voxel	= (back >= 0) ? (volume[ix][iy][back])					: 0.0;
+							double left_voxel	= (left < vol_size[0] - 1) ? (volume[left][iy][iz])		: 0.0;
+							double right_voxel	= (right >= 0) ? (volume[right][iy][iz])				: 0.0;
+							
+							double sum = up_voxel + down_voxel + front_voxel + back_voxel + left_voxel + right_voxel;
+							double interpolated = sum / 6;
+
+							if (interpolated >= options.threshold) {
+								viewOutput[w][h] = interpolation(interpolated, options);
 								break;
 							}
 						}
@@ -541,6 +619,7 @@ public:
 		/* DEBUG
 		*  Save the output image in a file without going through matlab
 		*/
+		std::ofstream ofs("./out.ppm", std::ios::out | std::ios::binary); //DEBUG IMAGE
 		ofs << "P6\n" << options.imageWidth << " " << options.imageHeight << "\n255\n";
 		for (uint32_t j = 0; j < options.imageHeight; ++j) {
 			for (uint32_t i = 0; i < options.imageWidth; ++i) {
@@ -552,7 +631,6 @@ public:
 		}
 
 		ofs.close();
-		oflog.close();
 		/* END DEBUG*/
 
 		outputs[0] = viewOutput;
